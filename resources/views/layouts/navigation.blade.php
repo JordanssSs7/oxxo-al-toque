4e5f32d80c9bc39f993c777ad97e836fe05bf6aa
IanRR <ian.rau.1721@gmail.com>
2026-05-22 18:33:41 -0500

    Modulo de inventarios y conexion con productos terminado

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    
+                    <x-nav-link :href="route('inventarios.index')" :active="request()->routeIs('inventarios.*')">
+                        {{ __('Inventario') }}
+                    </x-nav-link>
                 </div>
             </div>
 
